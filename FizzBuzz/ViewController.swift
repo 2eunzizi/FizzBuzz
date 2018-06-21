cb697058f8a83d488448bac9aaa8639e5b59f570
Michelle <2eunzizi@gmail.com>
2018-06-21 14:10:51 -0400

    updated

--- FizzBuzz/ViewController.swift
+++ FizzBuzz/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myLabel: UITextField!
+    @IBOutlet weak var numberLabel: UITextField!
+    
+    @IBAction func buttonClicker(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
